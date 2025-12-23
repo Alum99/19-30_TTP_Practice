c2724efa66642f51de032619dfc40cff87f4e25f
Alis <alisa.umyarova@gmail.com>
2025-12-23 14:40:59 +0700

    Update task_1.py

--- 23-24/task_1.py
+++ 23-24/task_1.py
@@ -3,7 +3,7 @@ from messages import MESSAGES
 from logger import logger
 from exceptions import DataNotSetError, InvalidValueError, OperationError, InputError
 
-msgs = MESSAGES["task1"]
+msgs = MESSAGES["task1"]   # берём подсловарь, относящийся к задаче 1
 
 def generate_array(size: int, min_v: int = 0, max_v: int = 50) -> list[int]:
     """Генерирует массив случайных чисел указанного размера.
@@ -17,7 +17,7 @@ def generate_array(size: int, min_v: int = 0, max_v: int = 50) -> list[int]:
         InvalidValueError: если size <= 0
     """
 
-    if size <= 0:
+    if size <= 0: # если размер неправильный
         raise InvalidValueError("Размер массива должен быть положительным")
     logger.info("Generating a random array")
     return [random.randint(min_v, max_v) for _ in range(size)]
@@ -42,8 +42,8 @@ def count_common_with_reverses(arr1: list[int], arr2: list[int]) -> int:
         OperationError: при возникновении ошибки в процессе подсчёта
     """
     
-    if not arr1 or not arr2:
-        raise DataNotSetError("Массивы не заданы или пусты")
+    if not arr1 or not arr2: # если хоть один массив пустой
+        raise DataNotSetError("Массивы не заданы или пусты") # выбрасывается ошибка
 
     count = 0
     used_pairs = []
@@ -59,12 +59,12 @@ def count_common_with_reverses(arr1: list[int], arr2: list[int]) -> int:
                         count += 1
                         logger.debug(f"Pair found: {pair}, quantity: {count}")
     except Exception as e:
-        raise OperationError(f"Ошибка выполнения подсчёта: {e}") from e
+        raise OperationError(f"Ошибка выполнения подсчёта: {e}") from     # e любые ошибки оборачиваются в OperationError
 
     return count
 
 
-def task1_fsm():
+def task1_fsm(): # Конечный автомат
     """
     Корутина конечного автомата задачи 1.
 
@@ -74,7 +74,7 @@ def task1_fsm():
 
     Состояния:
         NO_DATA   — массивы не заданы
-        HAS_DATA  — массивы заданы
+        HAS_DATA  — массивы заданы (введени или сгенерированы)
         HAS_RESULT — выполнен подсчёт
 
     Yields
@@ -83,13 +83,14 @@ def task1_fsm():
     Returns:
         None: Возврат в главное меню.
     """
+    # корутинный подход - состояние живёт внутри функции
 
     arr1 = None
     arr2 = None
     result = None
-    state = "NO_DATA"
+    state = "NO_DATA"  # начальное состояние автомата: данных нет
 
-    while True:
+    while True: # основной цикл
         print("\n" + msgs["title"])
         for option in msgs["menu"]:
             print(option)
@@ -97,25 +98,27 @@ def task1_fsm():
         # yield соответствует приёму входного символа автомата.
         # Корутины позволяют естественным образом описывать автоматы без внешних таблиц переходов.
         # Состояние хранится локально, переходы описываются явно
-        choice = yield
+        choice = yield 
         logger.info(f"task1 choice={choice}, state={state}")
 
-        if choice == "5":
+        if choice == "5": # выход в главное меню, завершаает корутину
             return
 
         # Каждая корутина инкапсулирует своё состояние
         # и самостоятельно управляет переходами между состояниями.
+        # обработка состояний конечного автомата
         if state == "NO_DATA":
-            if choice == "1":
+            if choice == "1": # ввод масисвов вручную
                 try:
                     arr1 = list(map(int, input("Первый массив: ").split()))
                     arr2 = list(map(int, input("Второй массив: ").split()))
-                    state = "HAS_DATA"
+                    state = "HAS_DATA"     # меняем состояние
                     logger.info("Arrays input manually")
                 except Exception as e:
                     print(msgs["input_error"])
                     logger.error(f"Input error: {e}")
-            elif choice == "2":
+                    
+            elif choice == "2":  # генерация массивов
                 try:
                     size1 = int(input("Размер первого массива: "))
                     size2 = int(input("Размер второго массива: "))
@@ -123,16 +126,16 @@ def task1_fsm():
                     arr2 = generate_array(size2)
                     print("Первый массив:", arr1)
                     print("Второй массив:", arr2)
-                    state = "HAS_DATA"
+                    state = "HAS_DATA"  # меняем состояние
                     logger.info("Arrays generated randomly")
                 except Exception as e:
                     print(msgs["input_error"])
                     logger.error(f"Generation error: {e}")
             else:
-                print(msgs["no_data"])
+                print(msgs["no_data"])  # неверный выбор
 
-        elif state in ("HAS_DATA", "HAS_RESULT"):
-            if choice == "3":
+        elif state in ("HAS_DATA", "HAS_RESULT"): # Состояния HAS_DATA и HAS_RESULT
+            if choice == "3": # выполнить расчет
                 try:
                     result = count_common_with_reverses(arr1, arr2)
                     state = "HAS_RESULT"
@@ -141,13 +144,15 @@ def task1_fsm():
                 except Exception as e:
                     print(msgs["no_data"])
                     logger.error(f"Calculation error: {e}")
-            elif choice == "4":
-                if result is None:
+                    
+            elif choice == "4": # показать результат
+                if result is None: # если результат еще не посчитан
                     print(msgs["no_data"])
                 else:
-                    print(f"Результат: {result}")
+                    print(f"Результат: {result}") # если результат есть
                     logger.info("Result displayed")
-            elif choice == "6":
+                    
+            elif choice == "6": # отключение логирования
                 logger.setLevel("CRITICAL")
                 print("Логирование отключено")
                 logger.critical("Logging disabled")
